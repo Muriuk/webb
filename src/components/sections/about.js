0b84c00a5c08107054bbcd21a5232d73acf50705
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2023-07-15 16:59:40 +0300

    Add files via upload

---
+++ src/components/sections/about.js
@@ -0,0 +1,205 @@
+import React, { useEffect, useRef } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
+import { srConfig } from '@config';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+
+const StyledAboutSection = styled.section`
+  max-width: 900px;
+
+  .inner {
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+`;
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    grid-gap: 0 10px;
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
+  .wrapper {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--green);
+
+    &:hover,
+    &:focus {
+      outline: 0;
+
+      &:after {
+        top: 15px;
+        left: 15px;
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1);
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 20px;
+      left: 20px;
+      z-index: -1;
+    }
+  }
+`;
+
+const About = () => {
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  const skills = ['Python', 'React js', 'Django', 'Next js', 'C [not deep]', 'Flask', 'MongoDB', 'SQL', 'Stata', 'R Programming', 'SPSS', 'Excel'];
+
+  return (
+    <StyledAboutSection id="about" ref={revealContainer}>
+      <h2 className="numbered-heading">About Me</h2>
+
+      <div className="inner">
+        <StyledText>
+          <div>
+            <p>
+              I am currently taking my bachelors at{' '}
+              <a href="https://egertonuniversity.ac.ke/" target="_blank" rel="noreferrer">
+                Egerton University
+              </a>{' '}
+              from 2020-2024. I was introduced to Data Science and Programming in my 1<sup>st</sup> semester and have been
+              interested ever since. Besides studying and programming, I love participating in
+              debates and general discussions. On an off day you'll find me bundled up in
+              a corner reading something related to tech.
+            </p>
+
+            <p>
+              I am always looking to learn new things. I am currently working on a few projects
+              related to <b>Software Development</b> and <b>Machine Learning</b>. At the same
+              time I am actively on the lookout for remote internships which I can pursue in field
+              of Data Science and Web Development.
+            </p>
+
+            <p>
+              I am a strong advocate for open source and I am always interested in working on new
+              projects with new people. Do check out my{' '}
+              <a
+                href="https://github.com/Muriuk?tab=repositories"
+                target="_blank"
+                rel="noreferrer"
+              >
+                repositories
+              </a>{' '}
+              and feel free to reach out on{' '}
+              <a
+                href="https://api.whatsapp.com/send?phone=254768258491&text=Hi"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Whatsapp
+              </a>{' '}
+              or{' '}
+              <a href="mailto:%20muriukikelvin27@gmail.com" target="_blank" rel="noreferrer">
+                email
+              </a>{' '}
+              if you would like to collaborate on any project related to Web Development and Data Science.
+            </p>
+
+            <p>Here are a few languages, frameworks, databases & softwares I’ve been working with recently:</p>
+          </div>
+
+          <ul className="skills-list">
+            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+          </ul>
+        </StyledText>
+
+        <StyledPic>
+          <div className="wrapper">
+            <StaticImage
+              className="img"
+              src="../../images/profile.jpeg"
+              width={500}
+              quality={100}
+              formats={['AUTO', 'WEBP', 'AVIF']}
+              alt="Headshot"
+            />
+          </div>
+        </StyledPic>
+      </div>
+    </StyledAboutSection>
+  );
+};
+
+export default About;
